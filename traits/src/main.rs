edb6e24d74b6f8006e95608809b9135b3b0cf3d5
Boris Paskhaver <boris.paskhaver@nyu.edu>
2025-01-12 17:20:48 -0500

    Clean out main.rs files in every Cargo project

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,11 +1,1 @@
-use std::ops::Add;
-
-fn add_two_numbers<T: Add<Output = T>>(a: T, b: T) -> T {
-    a + b
-}
-
-fn main() {
-    let integer_sum = add_two_numbers(1, 2);
-    let float_sum = add_two_numbers(1.5, 2.4);
-    println!("{integer_sum} and {float_sum}");
-}
+fn main() {}
