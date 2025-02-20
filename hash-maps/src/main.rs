054ada676acc512d84b517ca00b5ecc3ecf6dbac
Vinit Saini <vinit.saini@ibm.com>
2025-02-20 16:26:12 +0000

    Hashmap excercise completed

--- hash-maps/src/main.rs
+++ hash-maps/src/main.rs
@@ -1,1 +1,34 @@
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+
+    //creation of map with from construct
+    //from takes [] of tuples
+let mut sauces_to_meals= HashMap::from(
+    [
+        ("Ketchup", vec!["French Fries", "Burgers", "Hot Dogs"]),
+        ("Mayonnaise", vec!["Sandwiches", "Burgers", "Coleslaw"])
+    ]
+);
+
+//insert
+sauces_to_meals.insert("Mustard", vec!["Hot dog", "Burgers", "Pretzels"]);
+println!("{sauces_to_meals:#?}");
+
+//remove (unwrap the option)
+let result = sauces_to_meals.remove("Mayonnaise").unwrap();
+println!("removed vector values: {:?}", result);
+
+//get (match the option)
+let get_result = sauces_to_meals.get("Mustard");
+match get_result {
+    Some(v) => println!("get vector values: {:?}", v)
+    None => println!("key not founf in the map!")
+}
+
+
+//entry and or_insert
+sauces_to_meals.entry("Soy Sauce").or_insert(vec!["Sushi", "Dumplings"]);
+println!("{:#?}", sauces_to_meals);
+
+}
