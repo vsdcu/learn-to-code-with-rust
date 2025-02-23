4a5dea2857f69ee245a63fff4fe89730eba10927
Vinit Saini <vinit.saini2@mail.dcu.ie>
2025-02-23 08:00:17 +0000

    trait example

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,1 +1,42 @@
-fn main() {}
+use std::collections::HashMap;
+
+trait Accommodation {
+    fn get_description(&self);
+    fn book(&mut self, guest_name:String, nights: u32);
+}
+
+#[derive(Debug)]
+struct Hotel {
+    name: String,
+    reservations: std::collections::HashMap<String, u32>,
+}
+
+impl Hotel {
+    fn new(name: String) -> Self {
+        Self {
+            name: name, 
+            reservations: HashMap::new(),
+        }
+    }
+}
+
+impl Accommodation for Hotel {
+    fn get_description(&self) {
+        println!("Welcome to the hotel {}", self.name);
+    }
+
+    fn book(&mut self, guest_name:String, nights: u32) {
+        self.reservations.insert(guest_name, nights);
+    }
+}
+
+
+fn main() {
+
+    let mut hotel = Hotel::new("Reddisson".to_string());
+    hotel.get_description();
+    hotel.book(String::from("Dave"), 2);
+
+    println!("{hotel:#?}");
+
+}
